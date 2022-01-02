3b240e332016e9febce7130721cc5b58aa280c76
Kekelberi <alexei.gaponov@gmail.com>
2022-01-02 17:09:49 +0300

    save

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 function App() {
   return (
     <div className='wrapper'>
-      
+      <section className='exemple'>
+        <div className='exemple__conteier'>
+          <div className='exemple__header'>
+            <div className='exemple__sub-title'></div>
+            <h1 className='exemple__title'>yes</h1>
+            <div className='exemple__text'></div>
+          </div>
+          <div className="exemple__items">
+            <article className="exemple__item item-exemple">
+              <div className="item-exemple__body">
+                <h3 className="item-example__title">no</h3>
+                <div className="item-example__text"></div>
+              </div>
+            </article>
+
+          </div>
+        </div>
+      </section>
       <div className='app2'>two</div>
     </div>
   )
